f9c6653ba161f42b9c8942b3f95b9187dffc52b8
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-12-19 12:24:42 -0300

    user creation

--- App.js
+++ App.js
@@ -1,27 +1,72 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
+
 import firebase from './src/firebaseConnection';
 
-console.disableYellowBox=true;
-
-export default function App(){
-  const [nome, setNome] = useState('Carregando...');
-  const [idade, setIdade] = useState('');
+export default function App() {
+  const [name, setName] = useState('Loading...');
+  const [position, setPosition] = useState('');
   
-  async function dados(){
-    await firebase.database().ref('usuarios/2').on('value', (snapshot) => {
-      setNome(snapshot.val().nome);
-      setIdade(snapshot.val().idade);
-    });
-  }
-  useEffect(()=> {
-    dados();
+  useEffect(() => {
+    async function fetchData() {
+      await firebase.database().ref('type').set('Client');
+      await firebase.database().ref('type').remove();
+      await firebase.database().ref('users').child(3).update({
+        name: 'John',
+      });
+    }
+    fetchData();
   }, []);
 
-  return(
-    <View style={{marginTop: 25}}>
-      <Text style={{fontSize: 25}}>Olá {nome}</Text>
-      <Text style={{fontSize: 25}}>Idade {idade}</Text>
+  async function register() {
+    if (name !== '' && position !== '') {
+      let users = await firebase.database().ref('users');
+      let key = users.push().key;
+
+      users.child(key).set({
+        name: name,
+        position: position,
+      });
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Name</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        onChangeText={(text) => setName(text)}
+      />
+
+      <Text style={styles.text}>Position</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        onChangeText={(text) => setPosition(text)}
+      />
+
+      <Button
+        title="Add Employee"
+        onPress={register}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10,
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+  },
+});
