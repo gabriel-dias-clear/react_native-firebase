2b842c9c6f9e5890ffd7431acf3df33f91c1358b
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-12-20 11:30:53 -0300

    user config from firebase

--- App.js
+++ App.js
@@ -1,81 +1,28 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button, TextInput, FlatList } from 'react-native';
+import React, { useState, useEffect} from 'react';
+import { View, Text, StyleSheet, SafeAreaView, TextInput } from 'react-native';
 import firebase from './src/firebaseConnection';
+import Login from './src/Components/Login';
 
-export default function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [user, setUser] = useState('');
+export default function App(){
 
-  async function login() {
-    await firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(value => {
-        alert('Welcome, ' + value.user.email);
-        setUser(value.user.email);
-      })
-      .catch(error => {
-        alert('Something went wrong');
-      });
+  const [user, setUser] = useState(null)
+
+  if(!user){
+    return <Login/>
   }
 
-  async function logout() {
-    await firebase.auth().signOut();
-    setUser('');
-    alert('Successfully logged out');
-  }
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Email</Text>
-      <TextInput
-        style={styles.input}
-        underlineColorAndroid="transparent"
-        onChangeText={(text) => setEmail(text)}
-      />
-
-      <Text style={styles.text}>Password</Text>
-      <TextInput
-        style={styles.input}
-        underlineColorAndroid="transparent"
-        onChangeText={(text) => setPassword(text)}
-        secureTextEntry={true}
-      />
-
-      <Button
-        title="Login"
-        onPress={login}
-      />
-
-      <FlatList
-        keyExtractor={item => item.key}
-        data={users}
-        renderItem={({ item }) => (<Listing data={item} />)}
-      />
-
-      <Text>
-        {user}
-      </Text>
-      {user.length > 0 && <Button
-        title="Logout"
-        onPress={logout}
-      />}
-    </View>
+  return(
+    <SafeAreaView style={styles.container}>
+      <Text>Inside Tasks</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    margin: 10,
-  },
-  text: {
-    fontSize: 18,
-    marginBottom: 5,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 10,
-    marginBottom: 10,
-  },
-});
+  container:{
+    flex:1,
+    paddingTop: 25,
+    paddingHorizontal: 10,
+    backgroundColor: '#F2f6fc'
+  }
+})
---
+++ src/Components/Login/index.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect} from 'react';
+import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+
+import firebase from '../../services/firebaseConnection';
+
+export default function Login(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [type, setType] = useState('login')
+  
+    function handleLogin(){
+        if(type === 'login'){
+            const user = firebase.auth().signInWithEmailAndPassword(email, password).then(user => {
+                console.log(user)
+            }).catch(error => console.error(error))
+        }
+        else{
+            const user = firebase.auth().createUserWithEmailAndPassword(email, password).then(user => {
+                console.log(user.user)
+            }).catch(error => console.error(error))
+        }
+    }
+  
+    return(
+        <SafeAreaView style={styles.container}>
+            <TextInput
+                placeholder='Your email'
+                style={styles.input}
+                value={email}
+                onChangeText={ text => setEmail(text)}
+            />
+
+            <TextInput
+                placeholder='*******'
+                style={styles.input}
+                value={password}
+                onChangeText={ text => setPassword(text)}
+            />
+
+            <TouchableOpacity
+                style={[styles.handleLogin, { backgroundColor: type === 'login' ? 'blue' : '#141414'}]}
+                onPress={handleLogin}
+            >
+                <Text>{type === 'login' ? 'Access' : 'Register'}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                onPress={ () => setType(type => type === 'login' ? 'register' : 'login')}
+            >
+                <Text style={{textAlign: 'center'}}>{type === 'login' ? 'Create an account' : 'I already have an account'}</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        paddingTop: 40,
+        backgroundColor: '#F2f6fc',
+        paddingHorizontal: 10,
+      },
+      input:{
+        marginBottom: 10,
+        backgroundColor: '#FFF',
+        borderRadius: 4,
+        height: 45,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#141414'
+      },
+      handleLogin:{
+        alignItems: 'center',
+        justifyContent:'center',
+        height: 45,
+        marginBottom: 10,
+      },
+      loginText:{
+        color: '#FFF',
+        fontSize: 17,
+      }
+})
